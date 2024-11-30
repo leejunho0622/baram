d591346d1c7adb302470a20787f47543c0759350
leejunho0622 <lee49409780@gmail.com>
2024-11-30 16:17:58 +0900

    Modify variable name and Add collisionDetection()

--- baram.js
+++ baram.js
@@ -3,10 +3,10 @@ window.addEventListener("keydown", (e) => {
     const playerImg = document.querySelector("#player img");
     const playerInfo = player.getBoundingClientRect();
     
-    if(e.key === 'w' && playerInfo.top >= 0){
+    if(e.key === 'w' && playerInfo.top >= 0 && !collisionDetection(playerInfo, "up")){
         playerImg.src = "https://github.com/leejunho0622/baram/blob/develop/baram/pb_3x2.png?raw=true";
-        moveTop = playerInfo.top - 50;
-        player.style.top = moveTop + "px";
+        moveUp = playerInfo.top - 50;
+        player.style.top = moveUp + "px";
     }
     else if(e.key === 'a' && playerInfo.left >= 0){
         playerImg.src = "https://github.com/leejunho0622/baram/blob/develop/baram/pl_3x2.png?raw=true";
@@ -15,8 +15,8 @@ window.addEventListener("keydown", (e) => {
     }
     else if(e.key === 's' && playerInfo.top <= 550){
         playerImg.src = "https://github.com/leejunho0622/baram/blob/develop/baram/pf_3x2.png?raw=true";
-        moveRight = playerInfo.top + 50;
-        player.style.top = moveRight + "px";
+        moveDown = playerInfo.top + 50;
+        player.style.top = moveDown + "px";
     }
     else if(e.key === 'd' && playerInfo.left <= 600){
         playerImg.src = "https://github.com/leejunho0622/baram/blob/develop/baram/pr_3x2.png?raw=true";
@@ -24,3 +24,24 @@ window.addEventListener("keydown", (e) => {
         player.style.left = moveRight + "px";
     }
 });  
+
+function collisionDetection(px, direction) {
+    entity = ["mantis", "scorpion", "rabbit", "npc1", "npc2"];
+    if(direction === "up"){
+        for(let i=0; i<entity.length; i++){
+            const entityXY = document.getElementById(entity[i]).getBoundingClientRect();
+            if(px.top - 50 === entityXY.top)
+                return true;
+        }
+    }
+    else if(direction == "left"){
+
+    }
+    else if(direction == "down"){
+
+    }
+    else if(direction == "right"){
+
+    }
+    return false;
+}
